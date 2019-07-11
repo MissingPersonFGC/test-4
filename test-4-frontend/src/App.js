6f8932f76fc2bff9ac51e3b49eef376e9e09eec1
Corey Lanier <iam@coreylanier.com>
2019-07-11 13:09:40 -0400

    checking to see if CSV has already been uploaded, and also being able to concatenate CSVs

--- test-4-frontend/src/App.js
+++ test-4-frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CSVReader from "react-csv-reader";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
@@ -10,8 +9,8 @@ class App extends React.Component {
 
   parseCSV = data => {
     // create the empty array
-    const values = [];
-    console.log(data);
+    const { values } = this.state;
+    const newValues = [];
     // map through the csv arrays
     data.map((value, index) => {
       // we know that the first row is always the table header, so skip it.
@@ -22,13 +21,28 @@ class App extends React.Component {
           object[data[0][i]] = value[i];
         }
         // Push the new object into the array
-        values.push(object);
+        newValues.push(object);
       }
     });
-    // Put the new array on the state.
-    this.setState({
-      values
-    });
+    // If the array on the state is empty, set the state to the array within this function.
+    if (this.state.values.length === 0) {
+      this.setState({
+        values: newValues
+      });
+    } else {
+      // If the array is filled, checked to see by a unique value if this CSV has already been imported.
+      const index = values.findIndex(
+        v => v.ip_address === newValues[0].ip_address
+      );
+      console.log(index);
+      // If it hasn't, push the CSV into the existing array.
+      if (index === -1) {
+        const finalValues = values.concat(newValues);
+        this.setState({
+          values: finalValues
+        });
+      }
+    }
   };
 
   render() {
